785bec74bdf43b50cf0636469b322b2ee126a125
Delphi1960 <andrew.m.cherry@gmail.com>
2022-03-17 14:26:30 +0200

    add find location

---
+++ src/app/components/weather/ua_place.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+export default function ua_place() {
+  const searchLocation = [
+    {
+      location: "Одесса (home)",
+      coord: "lat=46.4725&lon=30.74136&altitude=42",
+      url: "",
+    },
+    {
+      location: "Одесса",
+      coord: "lat=46.4577&lon=30.7484&altitude=37",
+      url: "2-698738/Ukraine/Odessa",
+    },
+    {
+      location: "Одесса(горсовет)",
+      coord: "lat=46.48572&lon=30.74383&altitude=50",
+      url: "2-698740/Ukraine/Odessa/Odes’ka_Mis’krada/Odessa",
+    },
+    {
+      location: "Покровск",
+      coord: "lat=48.28198&lon=37.17585&altitude=199",
+      url: "2-704422/Ukraine/Donetsk/Krasnoarmiys’ka_Mis’krada/Pokrovsk",
+    },
+    {
+      location: "Киев",
+      coord: "lat=50.45466&lon=30.5238&altitude=187",
+      url: "2-703448/Ukraine/Kyiv_City/Kyiv",
+    },
+    {
+      location: "Измаил",
+      coord: "lat=45.35058&lon=28.83751&altitude=19",
+      url: "2-707308/Ukraine/Odessa/Izmail’skiy_Rayon/Izmail",
+    },
+    {
+      location: "Львов",
+      coord: "lat=49.83826&lon=24.02324&altitude=284",
+      url: "2-702550/Ukraine/Lviv/Lviv",
+    },
+  ];
+
+  return <div></div>;
+}
