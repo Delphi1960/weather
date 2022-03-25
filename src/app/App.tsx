3f25f5b978feac5a01eea66b481dbc055c93e3b5
Hlib Vyshnenko <novembrea@gmail.com>
2022-03-25 16:04:04 +0200

    restore moon phase route

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,6 +6,7 @@ import AppBar from './components/app-bar/AppBar'
 import Chart from './components/chart'
 import NotFound from './components/not-found'
 import SelectLocation from './components/select-location'
+import MoonPhase from './components/weather/MoonPhase'
 import WeatherNow from './components/weather/WeatherNow'
 import WeatherTable from './components/weather/WeatherTable'
 
@@ -19,6 +20,8 @@ export default function App() {
           <Route path="/" element={<WeatherNow />} />
           <Route path="/weathertable" element={<WeatherTable />} />
           <Route path="/chart" element={<Chart />} />
+          <Route path="/moonphase" element={<MoonPhase />} />
+
           <Route path="/selectlocation" element={<SelectLocation />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
